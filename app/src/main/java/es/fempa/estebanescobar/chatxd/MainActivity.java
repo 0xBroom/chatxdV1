cb08acf0332d00d480735e2cd9b46c9f44883eb5
Hopeman22 <juantaban@gmail.com>
2019-12-17 19:19:29 +0100

    Added chatkit library, new elements to the layout and the code implementation in the main.

--- app/src/main/java/es/fempa/estebanescobar/chatxd/MainActivity.java
+++ app/src/main/java/es/fempa/estebanescobar/chatxd/MainActivity.java
@@ -4,11 +4,96 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.stfalcon.chatkit.commons.models.IMessage;
+import com.stfalcon.chatkit.commons.models.IUser;
+import com.stfalcon.chatkit.messages.MessagesList;
+import com.stfalcon.chatkit.messages.MessagesListAdapter;
 
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    public static int max_userid; //Para tener controlado el número de ids creados.
+    public static int max_messid;
+    MessagesList messagesList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //MessagesListAdapter<Message> adapter = new MessagesListAdapter<>(senderId, imageLoader);
+        //messagesList = findViewById(R.id.messagesList);
+        //messagesList = ;
+        //messagesList.setAdapter(adapter);
+    }
+
+    public class Author implements IUser {
+
+        /*...*/
+
+        private String id;
+        private String name;
+        private String avatar;
+
+        public Author(){
+            super();
+            max_userid++;
+            String newid = max_userid+"";
+            id= newid;
+            name = "Default";
+            avatar = null;
+        }
+
+        @Override
+        public String getId() {
+            return id;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String getAvatar() {
+            return avatar;
+        }
+    }
+
+    public class Message implements IMessage {
+
+        /*...*/
+
+        private String id;
+        public String text;
+        public Author author;
+        public Date createdAt;
+
+        public Message(){
+            max_messid++;
+            id = max_messid+"";
+            text = "Hola";
+            Author author = new Author();
+            createdAt = new Date();
+        }
+
+
+        @Override
+        public String getId() {
+            return id;
+        }
+
+        @Override
+        public String getText() {
+            return text;
+        }
+
+        @Override
+        public Author getUser() {
+            return author;
+        }
+
+        @Override
+        public Date getCreatedAt() {
+            return createdAt;
+        }
     }
 }
