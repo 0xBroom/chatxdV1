c8fd2205fca89e0c721dd4b7574f68f1e7b07396
JonRevo <jonandres97@hotmail.com>
2019-12-17 20:02:33 +0100

    Created Author and Message class

--- app/src/main/java/es/fempa/estebanescobar/chatxd/Message.java
+++ app/src/main/java/es/fempa/estebanescobar/chatxd/Message.java
@@ -6,6 +6,24 @@ import com.stfalcon.chatkit.commons.models.IUser;
 import java.util.Date;
 
 public class Message implements IMessage {
+
+    private String id;
+    private String text;
+    private IUser user;
+
+    public Message(){
+        id = null;
+        text = "";
+        user = null;
+    }
+
+    public Message(String id, String text, IUser user){
+        this.id = id;
+        this.text = text;
+        this.user = user;
+    }
+
+
     @Override
     public String getId() {
         return null;
