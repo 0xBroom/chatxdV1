73cf03b8142e72c2bbb0b79a67ce05a1296c8b9d
JonRevo <jonandres97@hotmail.com>
2020-01-07 18:06:13 +0100

    Added ConfigActivity and ChatActivity

---
+++ app/src/main/java/es/fempa/estebanescobar/chatxd/ConfigActivity.java
@@ -0,0 +1,14 @@
+package es.fempa.estebanescobar.chatxd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+}
