954fb272892ad98c8286f8fb96f2747f3ad44b30
Hopeman22 <juantaban@gmail.com>
2020-01-07 20:13:44 +0100

    Added config activity.

--- app/src/main/java/es/fempa/estebanescobar/chatxd/ConfigActivity.java
+++ app/src/main/java/es/fempa/estebanescobar/chatxd/ConfigActivity.java
@@ -3,12 +3,24 @@ package es.fempa.estebanescobar.chatxd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class ConfigActivity extends AppCompatActivity {
+    Switch s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+        s = (Switch) findViewById(R.id.switch1);
+    }
+
+    public void connect(View v){
+        if(s.isChecked()){
+            //Cliente
+        }else{
+            //Servidor
+        }
     }
 }
