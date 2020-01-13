8c883b0f47a32d44da53186a81f04c2afce54650
JonRevo <jonandres97@hotmail.com>
2020-01-13 16:36:03 +0100

    Created onClickCancel and onClickswitch

--- app/src/main/java/es/fempa/estebanescobar/chatxd/ConfigActivity.java
+++ app/src/main/java/es/fempa/estebanescobar/chatxd/ConfigActivity.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 
 public class ConfigActivity extends AppCompatActivity {
     Switch s;
-    private  TextView test;
+    private  TextView info;
     private EditText etIP;
     private EditText etPort;
     private Hilos h;
@@ -23,28 +23,50 @@ public class ConfigActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
         s = findViewById(R.id.switch1);
-        test = findViewById(R.id.test);
+        info = findViewById(R.id.test);
         etIP = findViewById(R.id.etIP);
         etPort = findViewById(R.id.etPort);
         h = new Hilos(this);
     }
 
-    public void connect(View v){
+    public void onClickConnect(View v){
         if(s.isChecked()){
             //Cliente
-            h.openClient(Integer.parseInt(etPort.getText().toString()),etIP.getText().toString());//
+            h.openClient(Integer.parseInt(etPort.getText().toString()),etIP.getText().toString());
         }else{
             //Servidor
             h.openServer(Integer.parseInt(etPort.getText().toString()));
         }
     }
 
+    public void onClickCancel(View v){
+
+    }
+
+    public void onClickSwitch(View v){
+        if(s.isChecked()){
+            //Cliente
+            etIP.setEnabled(true);
+        }else{
+            //Servidor
+           etIP.setEnabled(false);
+        }
+    }
+
+    public boolean validateInputs(){
+        boolean salida = true;
+
+        return salida;
+    }
+
+
+
     public void changeText(String text){
         final String aux = text;
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                test.setText(aux);
+                info.setText(aux);
             }
         });
     }
